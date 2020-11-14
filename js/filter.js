9ba0aa144b27b6807800401ca93357a53626a720
Евгений <lazarev.haven@gmail.com>
2020-11-14 16:39:23 +0300

    исправил ошибки линтера

--- js/filter.js
+++ js/filter.js
@@ -4,13 +4,13 @@
   const filterData = function (adsData) {
     let housingValue = `palace`;
 
-    const HousingTypeFilter = function (ads) {
+    const housingTypeFilter = function (ads) {
       const sameHousingType = ads.filter(function (ad) {
         return ad.offer.type === housingValue;
       });
 
       return sameHousingType;
-    }
+    };
 
     const housingType = document.querySelector(`#housing-type`);
 
@@ -18,13 +18,13 @@
       housingValue = housingType.value;
       let ads = adsData;
 
-      ads = HousingTypeFilter(ads);
+      ads = housingTypeFilter(ads);
 
       const oldPins = window.card.mapPins.querySelectorAll(`.map__pin:not(.map__pin--main)`);
 
       for (const oldPin of oldPins) {
         oldPin.parentElement.removeChild(oldPin);
-      };
+      }
 
       window.card.mapPins.appendChild(window.pins.getPins(ads));
       window.card.addCard(ads);
